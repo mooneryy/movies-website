f84d74d14c0fa822502d81560977d98894e378d2
mooneryy <yelisyeyevaa@gmail.com>
2024-03-13 13:52:03 +0200

    +

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
   header.classList.toggle('shadow', window.scrollY > 0);
 });
 
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+menu.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
 
 var swiper = new Swiper(".home", {
       spaceBetween: 30,
